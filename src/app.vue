fbeabbf19fd1930ceab6715ea85d58194354cc54
xiaoyan <3072861626@qq.com>
2023-06-19 18:17:59 +0800

    update: resume content

--- src/app.vue
+++ src/app.vue
@@ -38,7 +38,8 @@ const {
       <MainItem title="项目" more="https://github.com/2113ic?tab=repositories">
         <div class="project-container">
           <ProjectItem
-            v-for="project in projects"
+            v-for="(project, i) in projects"
+            :class="`item${i + 1}`"
             :name="project.name"
             :key="project.name"
             :site="project.site"
@@ -46,14 +47,29 @@ const {
             :desc="project.desc"
           >
             <template v-if="project.name === 'Dot'">
+              <p>
+                现有：htmlPlus（html增强）、Banner、LightTip、Pagination、FormValidator、TabSwitcher。
+              </p>
+              <p>htmlPlus介绍：</p>
+              <p>
+                该项目通过实现4个指令增强原生开发体验。主要通过html属性传入的参数实现特定的功能。
+              </p>
               <ul>
-                <li>HtmlPlus 通过实现类似vue的指令增强原生开发体验</li>
-                <li>Pagination 分页器</li>
-                <li>FormValidator 表单验证器</li>
-                <li>TabSwitcher Tab切换器</li>
-                <li>LightTip 轻提示</li>
-                <li>Banner 轮播图</li>
+                <li>data-ref: 获取元素。</li>
+                <li>data-event: 为元素绑定事件。</li>
+                <li>data-bind：绑定数据。</li>
+                <li>data-clone: 根据用户传入的数据克隆节点。</li>
               </ul>
+              <p>
+                <b>遇到难点：</b>
+                如何正确地根据bind、clone指令传入的dataKey、cloneDataKey找到对应的数据。
+              </p>
+              <p>
+                <b>解决方案：</b>
+                在clone节点时通过new
+                Function方式获取数据，并把子项的数据提前绑定到子节点上。在bind指令绑定数据时，使用node.closest、new
+                Function的方式获取正确的数据绑定在元素上。
+              </p>
             </template>
 
             <template v-if="project.name === '个人博客'">
@@ -162,12 +178,36 @@ main {
   .project-container {
     display: grid;
     gap: 12px;
-    grid-template-columns: 1fr 1fr;
+    align-items: start;
+    grid-template-columns: [c1] 1fr [c2] 1fr [c3];
+    grid-template-rows: [r1] 1fr [r2] auto [r3];
+
+    .item1 {
+      grid-column-start: c1;
+      grid-column-end: c3;
+      grid-row-start: r1;
+      grid-row-end: r2;
+    }
+
+    .item2 {
+      grid-column-start: c1;
+    }
+
+    .item3 {
+      grid-row-start: r2;
+      grid-column-start: c2;
+    }
   }
 
   @media (max-width: 640px) {
     .project-container {
       grid-template-columns: 1fr;
+      grid-template-rows: repeat(3, auto);
+      
+      .project-item {
+        grid-column: auto;
+        grid-row: auto;
+      }
     }
   }
 
